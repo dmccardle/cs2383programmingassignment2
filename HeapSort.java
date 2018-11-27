e9c815765a20ba7f9f421a9bd35896c5c45cf3a4
dmccardle <danny.mccardle@unb.ca>
2018-11-27 16:51:41 -0400

    Implemented QuickSort. Added printing to all files

--- HeapSort.java
+++ HeapSort.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 public class HeapSort {
 
-    public static void main() {
+    public static void main(String[] args) {
         int[] A = {3, 6, 10, 18, 8, 7, 25, 40};
         sort(A);
         show(A); // display the sorted result A
@@ -11,7 +13,7 @@ public class HeapSort {
     }
 
     public static void show(int[] A) {
-        
+        System.out.println(Arrays.toString(A));
     }
     // add all other functions you need
 }
--- QuickSort.java
+++ QuickSort.java
@@ -1,17 +1,43 @@
+import java.util.Arrays;
+
 public class QuickSort {
 
-    public static void main() {
+    public static void main(String[] args) {
         int[] A = {3, 6, 10, 18, 8, 7, 25, 40};
-        sort(A);
+        quickSort(A, 0, A.length-1);
         show(A); // display the sorted result A
     }
 
-    public static void sort(int[] A) {
-        
+    public static void quickSort(int[] A, int i, int j) {
+        if(i < j) {
+            int h = partition(A, i, j);
+            quickSort(A, i, h-1);
+            quickSort(A, h+1, j);
+        }
     }
 
     public static void show(int[] A) {
-        
+        System.out.println(Arrays.toString(A));
     }
+
     // add all other functions you need
+    public static int partition(int[] A, int i, int j) {
+        int val = A[i];
+        int h = i;
+        for(int k = i+1; k < j; k++) {
+            if(A[k] < val) {
+                h++;
+                swap(A, h, k);
+            }
+        }
+        swap(A, i, h);
+
+        return h;
+    }
+
+    public static void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
 }
