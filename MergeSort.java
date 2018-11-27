3659cfa14c5ee237f791d107d207ba36b2a4220b
dmccardle <danny.mccardle@unb.ca>
2018-11-27 16:21:22 -0400

    Initial push, added files for part 1

---
+++ MergeSort.java
@@ -0,0 +1,17 @@
+public class MergeSort {
+
+    public static void main() {
+        int[] A = {3, 6, 10, 18, 8, 7, 25, 40};
+        sort(A);
+        show(A); // display the sorted result A
+    }
+
+    public static void sort(int[] A) {
+        
+    }
+
+    public static void show(int[] A) {
+        
+    }
+    // add all other functions you need
+}
